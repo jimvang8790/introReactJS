c0b79d7759de47f7c4545e908c0dcfbd51a66122
Jim Vang <vangjim15@gmail.com>
2017-07-27 14:20:08 -0500

    comparison add

--- app.js
+++ app.js
@@ -90,3 +90,25 @@ class Random extends React.Component {
     return <h1>The number is {n}!</h1>;
   }
 }
+
+// conditional statement inside of a render() function
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TodaysPlan extends React.Component {
+  render() {
+    let task;
+    if (!apocalypse) {
+      task = 'learn React.js'
+    } else {
+      task = 'run around'
+    }
+
+    return <h1>Today I am going to {task}!</h1>;
+  }
+}
+
+ReactDOM.render(
+	<TodaysPlan />,
+	document.getElementById('app')
+);
