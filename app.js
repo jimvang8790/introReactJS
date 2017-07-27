4ea6b1ca98b65231c5d049a7da1260c5edbe6dad
Jim Vang <vangjim15@gmail.com>
2017-07-27 14:27:53 -0500

    add event listener to componet

--- app.js
+++ app.js
@@ -141,3 +141,43 @@ get name() {
 }
 
 ReactDOM.render(<MyName />, document.getElementById('app'));
+
+// Render functions often contain event listeners. Here's an example of an event listener in a render function:
+render() {
+  return (
+    <div onHover={myFunc}>
+    </div>
+  );
+}
+
+// In React, you define event handlers as methods on a component class. Like this:
+class MyClass extends React.Component {
+  myFunc() {
+    alert('Stop it.  Stop hovering.');
+  }
+
+  render() {
+    return (
+      <div onHover={this.myFunc}>
+      </div>;
+    );
+  }
+}
+
+// Another oneimport React from 'react';
+import ReactDOM from 'react-dom';
+
+class Button extends React.Component {
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
+  }
+
+  render() {
+    return <button onClick={this.scream}>AAAAAH!</button>;
+  }
+}
+
+ReactDOM.render(
+<Button />
+document.getElementById('app')
+);
